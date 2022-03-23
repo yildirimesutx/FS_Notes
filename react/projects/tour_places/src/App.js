fa4aec364b6430dda237942f2dfe0bab02c304ab
mesut <yildirimesutcw@gmail.com>
2022-03-23 23:40:47 +0300

    solution

---
+++ react/projects/tour_places/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+
+const App = ()=>{
+  return(
+    <div className='App'>
+   <Navbar/>
+   <Header/>
+   <Main/>
+
+    </div>
+  )
+}
+
+export default App;

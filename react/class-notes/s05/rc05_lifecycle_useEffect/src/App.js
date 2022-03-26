f5c0dc9717b29955233ab493876c18ea049cd76a
mesut <yildirimesutcw@gmail.com>
2022-03-26 15:07:35 +0300

    effect

---
+++ react/class-notes/s05/rc05_lifecycle_useEffect/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import ClassComponent from './components/ClassComponent'
+import UseEffectComp from './components/UseEffectComp'
+import Users from './components/Users'
+import UseStateComp from './components/UseStateComp'
+
+function App() {
+  const [isVisible, setIsVisible] = useState(true)
+  return (
+    <div className="App">
+      {/* <h2>LifeCycles</h2> */}
+      {/* <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button>
+      {isVisible && <ClassComponent />} */}
+      {/* <UseStateComp/> */}
+      {/* <UseEffectComp/> */}
+      {/* {isVisible && <UseEffectComp />}
+      <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button> */}
+
+      <Users/>
+    </div>
+  )
+}
+
+export default App

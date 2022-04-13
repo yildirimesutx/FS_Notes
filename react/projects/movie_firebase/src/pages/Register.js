dbea7f3d5fd0682cb122873182158503e53c6857
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-04-13 15:09:26 +0300

    firebase

--- react/projects/movie_firebase/src/pages/Register.js
+++ react/projects/movie_firebase/src/pages/Register.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { createUser } from '../auth/firebase'
+import {useNavigate} from "react-router-dom"
+
 
 const Register = () => {
+
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const navigate = useNavigate()
+
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+ 
+  createUser(email,password, navigate)
+  console.log(firstName, lastName)
+}
+
+
   return (
-    <div>Register</div>
+    <div className='d-flex justify-content-center'>
+    <div className='form-image'>
+      <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+    </div>
+   <div className='register-form'>
+       <h1 className='form-title display-3'>Register</h1>
+       <form id='register' onSubmit={handleSubmit}>
+         <div className='mb-3'>
+          <label htmlFor="first-name" className='form-label'>First Name</label>
+          <input type="text" className='form-control' id='first-name' placeholder='Enter your name...' onChange={(e)=>setFirstName(e.target.value)} />
+          </div>
+
+          <div className='mb-3'>
+          <label htmlFor="last-name" className='form-label'>Last Name</label>
+          <input type="text" className='form-control' id='last-name' placeholder='Enter your lastname...' onChange={(e)=>setLastName(e.target.value)} />
+          </div>
+
+          <div className='mb-3'>
+          <label htmlFor="email" className='form-label'>Email</label>
+          <input type="email" className='form-control' id='email' placeholder='Enter your email...' onChange={(e)=>setEmail(e.target.value)}/>
+          </div>
+
+          <div className='mb-3'>
+          <label htmlFor="password" className='form-label'>Password</label>
+          <input type="password" className='form-control' id='first-name' placeholder='Enter your password...' onChange={(e)=>setPassword(e.target.value)} required/>
+          </div>
+
+      <input className='btn btn-primary form-control' value="Register" type="submit" />
+
+       </form>
+   </div>
+
+
+
+    </div>
   )
 }
 

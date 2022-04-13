acb1831caec8e1b67f94555012f9a5775f1a243d
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-04-13 17:54:06 +0300

    axios

--- react/projects/movie_firebase/src/pages/Main.js
+++ react/projects/movie_firebase/src/pages/Main.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
-  )
-}
+    <div>
+
+ { const getUser = async () =>{
+  
+    const response = await axios.get('https://api.themoviedb.org/3/movie/550?api_key=5dc933df20d36ea2b570db659cb05285');
+    console.log(response);
+  }}
+
+
+
+    </div>
+  
+
 
 export default Main

1223634404168f061ba9afbd835ff6031a034310
mesut <yildirimesutcw@gmail.com>
2022-04-19 00:25:15 +0300

    redux

---
+++ react/class-notes/s12-redux2_starter/src/pages/News.js
@@ -0,0 +1,79 @@
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { CardMedia } from "@mui/material";
+import axios from "axios";
+import { useEffect } from "react";
+import { clearLoading, setLoading } from "../redux/actions/appActions";
+import {useDispatch} from "react-redux"
+
+const News = () => {
+
+  const dispatch = useDispatch()
+  const url =
+    "https://newsapi.org/v2/everything?" +
+    "q=Apple&" +
+    "from=2022-04-18&" +
+    "sortBy=popularity&" +
+    "apiKey=1a1a999e0d7240a6bd2dead87bcca78e";
+
+  const getNews = async () => {
+   try {
+     dispatch(setLoading())
+    const { data } = await axios.get(url);
+    console.log(data.articles); 
+   } catch (error) {
+     console.log(error)
+    } finally{
+      dispatch(clearLoading())
+    }
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <Box
+      xs={{ d: "flex" }}
+      display="flex"
+      alignItems="center"
+      justifyContent="space-evenly"
+      flexWrap="wrap"
+    >
+      {[1, 2, 3, 4].map((item, index) => (
+        <Card sx={{ maxWidth: 345, m: 5, maxHeight: 600 }} key={index}>
+          <CardMedia
+            component="img"
+            height="250"
+            image={
+              item?.urlToImage ??
+              "https://ichef.bbci.co.uk/news/976/cpsprodpb/5A8B/production/_122497132_tesla.png"
+            }
+            alt="img"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item?.title ?? "Tesla disables gaming while driving feature"}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item?.content ??
+                "It follows an inquiry into Passenger Play, which allowed games to be played while a car was moving."}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small" href={item?.url} target="_blank">
+              Detail
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Box>
+  );
+};
+
+export default News;
---
+++ react/class-notes/s12-redux2_starter/src/redux/reducers/newsReducer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const newsReducer = () => {
+  return (
+    <div>newsReducer</div>
+  )
+}
+
+export default newsReducer

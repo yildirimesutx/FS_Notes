b2559296c3edb25bc44a7c47fa1fe05217796da2
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-05-13 17:55:25 +0300

    oop notes

--- django/class-notes/S02-PYTHON_OOP/OOP_post_class.py
+++ django/class-notes/S02-PYTHON_OOP/OOP_post_class.py
@@ -46,20 +46,216 @@ print(person1.name)
 # isim ve age gibi instance ait özellikler instance attribute olarak  tanımlanması gerekir
 
 # SELF keyword
+# class içindeki func lara methot diyoruz
+
+
+# class Person:
+#     name = "mesut"
+#     age = 34
+
+#     def test(self):
+#         print("test")
+
+# person1= Person()
+# # person1.test()        #bu örnekte test() içinde self koymadık, aşağıdaki örneği yapabilmek için ekledik 
+
+# # yukarıdaki instance methotu çağırdığımızda çalışmadı çünkü  class ile instance arasında bağ kurulmadı, instance lar fiziki elamanlar class yapısı soyut, bu baplantıyı SELF keywordu ile yapıyoruz, self yerine farklı keyword yazılabilir fakat bestpractiv bu şekilde
+
+
+# Person.test(person1)
+
+
+# instance lara methot tanımlama, yani bir bir func yazıyoruz bu methot ile class yapısında genel bir yapı oluşuyor, ve bu methotta belirlediğimiz değerler ile  instance tanımlarken girilmesi ile yeni instanceları dinamik olarak oluşturuyoruz. aşağıdaki örnekte
+
+# class Personel:
+
+#     def set_details(self, name, age): #bu methot ile tanımlanacak yeni instanceler için bir yapı oluşturuldu, instanceleri ilgilendiren attributeler için
+#         self.name = name
+#         self.age = age
+
+#     def get_details(self):
+#         print(self.name, self.age) 
+
+
+# per = Personel()
+
+# per.set_details("mesut yildirim", 34)  # bu methot ile tanımlama yapıyoruz
+
+# per.get_details()  # bu methot ile tanımlanan değerleri alıyoruz
+
+
+# statik method
+
+# classtan ve instance dan bağımsız kendi başına çalışan func
+
+
+# class Personel:
+
+#     company = "Clarusway"
+
+#     def set_details(self, name, age): 
+#         self.name = name
+#         self.age = age
+
+#     def get_details(self):
+#         print(self.name, self.age)
+
+#     @staticmethod  # class ve instance dan bağımsız olduğu için self kullanmadık, django da statik method kullanmayacağız, djangonun class yspılarını kullanacağız
+#     def salute():
+#         print("hi there!")   
+
+# person1 = Personel()
+# person1.salute()
+
+# SPECIAL METHODS
 
 class Person:
-    name = "mesut"
-    age = 34
+    company = "clarusway"
 
-    def test():
-        print("test")
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    # def __str__(self):
+    #     return f"{self.name} {self.age}"
 
+    def get_details(self):
+        print(self.name, self.age)  
+
+person1 = Person("mesut", 33)  # yukarıda verilen tanımlamada set_details method unda tanımlamaları aldık ve bu methodu kullanarak instance tanımlamaları yaptık,  burada ise ilk instance tanımlarken ilgili özellikler içerisine yazılıyor. constracter yapıyoruz
+# person1.get_details() 
+
+liste = [4, 2, 3, 5 ,8]
+print(liste)  #burada class yapıyı çağırdığımızda gösteriyor, fakat person1 çağırdığımızda göstermiyor
+
+print(person1)   # bu şekilde çağırdığımızda instance içindeki attributleri göstermedi, bunun için str yapısını lullanıyoruz.
+
+# print(person1.company)
+
+
+# OOP’nin 4 Ana Prensibi: Encapsulation, Inheritence, Abstraction, Polymorphism
+
+# encapsulation and abstraction
+
+# encapsulation da private yapılar kullanılmaktadır. Bunn sebebi dışarıdan bir müdahale ile yapıyı bozmamaktadır. Pythonda da private yapı kullanılmamktadır, ulaşılmamsı istenilen değerlerin önüne _ koyulmuş _id bu şekilde, bu _id şekilde ifadelerde bu yapının değişmemesi gerektiği değişirse eğer yapının bozulacağını ifade eder, sonra gelişen süreçte bu tek çizgiyi __ iki kere yazılması ile ulaşılıp değiştirilmeyecek hale gelmiştir. 
+#
+ 
+# class Person:
+#     company = "clarusway"
+
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self._id = 5000
+#         self.__id2 = 4000
+    
+#     def __str__(self):
+#         return f"{self.name} {self.age}"
+
+#     def get_details(self):
+#         print(self.name, self.age)  
+
+# person1 = Person("aeron", 34)
+# # print(person1._id)
+# # print(person1.__id2)
+# print(person1._Person__id2) # bu şekilde yazıldığında kuralı deliyorve ilgili değere ulaşabiliyoruz
+
+# Abstraction
+
+# kod parçaçığı çalışırken arka tarafta nasıl ve ne şekilde işlemlerin yapıldığını bilinmemesinin istenilmesi, girdi çıktıya odaklanma 
+
+
+
+
+# inheritance and polymorphism
+
+class Person:
+    company = "clarusway"
+
+    def __init__(self, name, age, surname):
+        self.name = name
+        self.age = age
+        self.surname = surname
         
+       
+    
+    def __str__(self):
+        return f"{self.name} {self.age}"
+
+    def get_details(self):
+        print(self.name, self.age, self.surname) 
+
+
+# class Employee(Person):   # burada Person clasını miras aldık onun tüm attributleri alabiliyoruz
+#      pass
+
+# emp1 = Employee("Barry", 24)
+# emp1.get_details()
+
+# class Employee(Person):   # burada Person clasını miras aldık onun tüm attributleri alabiliyoruz
+#         def __init__(self, name, age, path, surname):
+#             # self.name = name
+#             # self.age = age    # BURADA BELİRTİLEN AGE VE NAME PARENT DAN GELDİ, PARENT İN ATTİBUTLERİ ÇOK SAYIDA OLABİLİR BU SEBEPLE TEK TEK YAZILMASI UYGUN DEĞİL, BU SEBEPLE super() keywordunu kullanıyoruz
+#             super().__init__(name, age, surname) #parent ta ulaşmak için super() kullanıldı
+#             self.path = path
+
+
+
+# emp1 = Employee("Barry", 24, "FS", "yıldırım")
+# emp1.get_details()  
+
+# yukarıda tanımladığımız path özelliği yazdırıldığı zaman gelmiyor, çünkü get_details miras alındığı classa ait, miras alınan clasta istenilen değerlerin alınmasına polymorphism deniyor
+
+# polymorphism
+# miras alınan clasta istenilen değerlerin alınmasına polymorphism deniyor
+        
+# class Employee(Person):   # burada Person clasını miras aldık onun tüm attributleri alabiliyoruz
+#         def __init__(self, name, age, path, surname):
+           
+#             super().__init__(name, age, surname) 
+#             self.path = path
+
+#         # override işlemi deniliyor, miras alındıktan sonra istenilen attributle ile işimize gelenleri kullanıyoruz
+
+#         def get_details(self):
+#             # print(self.name, self.age, self.path) # 1.yol bu şekilde de yazılabilir, kullanmak istediklerimiz miras alınanlar ve yeni eklediğimiz özellik
+#             super().get_details() #2.yol da bu şekildegwt_details çağırdık ve yeni eklenmesi gereken özelliği dahil ettik print ile
+#             print(self.path)
 
 
 
 
+# emp1 = Employee("Barry", 24, "FS", "yıldırım")
+# emp1.get_details()  
+
+# multi inheritance 
+
+class Lang:
+    def __init__(self, langs):
+         self.langs=langs
+
+    # def display_langs(self):
+    #     print(self.langs)     
 
 
+class Employee(Person, Lang):   
+        def __init__(self, name, age, path, surname, langs):
+           
+            super().__init__(name, age, surname) 
+            self.path = path
+            Lang.__init__(self, langs) # burada ikinci gelen classa bu şekilde ulaştık
 
+      
+
+        def get_details(self):
+           
+            super().get_details()
+            print(self.path)
+            print(self.langs) # gösterilmesi için burada tanımladık, 2. yol display ile tanımlayıp tekrar disolay ile çağırabilirdik
+
+
+emp1 = Employee("Barry", 24, "FS", "yıldırım", ["python", "js"])
+emp1.get_details()
+# emp1.display_langs() 
+print(Employee.mro())  # bu komut bize class ın hangi classlardan türetildiğini bize veriyor.
 

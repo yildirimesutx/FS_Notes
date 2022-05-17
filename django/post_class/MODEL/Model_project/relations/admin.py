62081cf6dfc57d351856ce5a901100431ad6e7b3
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-05-17 16:38:12 +0300

    django model post_class

---
+++ django/post_class/MODEL/Model_project/relations/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Creator, Language, Frameworks, Developers
+
+# Register your models here.
+
+admin.site.register(Creator)
+admin.site.register(Language)
+admin.site.register(Frameworks)
+admin.site.register(Developers)

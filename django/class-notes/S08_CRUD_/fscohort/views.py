e3721b90a879601e62de8426ff283c2e1dc83a51
mesut <yildirimesutcw@gmail.com>
2022-05-22 15:00:45 +0300

    update, delete and detail pages

--- django/class-notes/S08_CRUD_/fscohort/views.py
+++ django/class-notes/S08_CRUD_/fscohort/views.py
@@ -16,14 +16,64 @@ def student_list(request):
 
 
 def student_add(request):
-    form = StudentForm() #boş render ettik
-    if request.method == 'POST':
-        print(request.POST)
-        form = StudentForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("list")
+
+    form = StudentForm()
+    print(request.POST)
+
+    if request.method == "POST":
+       form = StudentForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect("list")
+
+
     context = {
-        'form' : form,
-    }        
-    return render(request, 'fscohort/student_add.html', context)
+    'form' : form
+    }
+
+    return render(request, "fscohort/student_add.html", context)
+
+
+def student_update(request, id):
+    student = Student.objects.get(id=id)
+    form = StudentForm(instance = student)
+    
+    if request.method == "POST":
+       form = StudentForm(request.POST, instance=student)
+       print(form)
+       if form.is_valid():
+           form.save()
+           return redirect("list")
+
+
+    context = {
+          "form" : form
+    }   
+
+    return render(request, "fscohort/student_update.html", context)
+
+def student_delete(request, id):
+    student = Student.objects.get(id=id)
+
+    if request.method == "POST":
+        student.delete()
+        return redirect("list")
+
+    context = {
+       "student":student
+    }
+
+
+    return render(request, "fscohort/student_delete.html", context)
+
+
+def student_detail(request, id):
+    student = Student.objects.get(id=id)
+
+    context = {
+       "student":student
+    }
+
+
+    return render(request, "fscohort/student_detail.html", context)
+       

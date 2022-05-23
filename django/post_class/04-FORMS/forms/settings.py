10c1f8bee1d89349d3eb2c36ca7aeccef95137bf
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-05-23 17:18:04 +0300

    forms post class

--- django/post_class/04-FORMS/forms/settings.py
+++ django/post_class/04-FORMS/forms/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

28a20152029e38218abf10439dd02c6efdd8742d
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-05-25 16:26:05 +0300

    class update delete add pages

--- django/post_class/06-CB_VIEWS/student/urls.py
+++ django/post_class/06-CB_VIEWS/student/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeView, StudentListView, StudentDetailView
+from .views import HomeView, StudentListView, StudentDetailView, StudentCreateView,StudentUpdateView, StudentDeleteView
 # from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -7,5 +7,10 @@ urlpatterns = [
     #  path('', TemplateView.as_view(template_name= "student/home.html"), name="home")
     path('', HomeView.as_view(), name="home"),
     path('student/', StudentListView.as_view(), name="list"),
-    path('detail/<int:id>', StudentDetailView.as_view(), name="detail")
+    path('detail/<int:id>', StudentDetailView.as_view(), name="detail"),
+    path('student_add/', StudentCreateView.as_view(), name='add'),
+    path('update/<int:pk>', StudentUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', StudentDeleteView.as_view(), name='delete'),
+    
+    
 ]
--- django/post_class/06-CB_VIEWS/student/views.py
+++ django/post_class/06-CB_VIEWS/student/views.py
@@ -1,22 +1,54 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 from .models import Student
+from .forms import StudentForm
 
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 
-
-
+# template görüntüleme
 class HomeView(TemplateView):
    template_name = "student/home.html"
 
+
+# liste görüntüleme
 class StudentListView(ListView):
     model = Student
     context_object_name = 'students'
 
+# oluşturulan listenin detaylarını görme
 class StudentDetailView(DetailView):
     model = Student
-    pk_url_kwarg = 'id'
+    pk_url_kwarg = 'id'  # default olarak urls de <int:pk> kullanılması gerekiyor, eğer pk yerine id kullanılması isteniyorsa bu satır yazılmasıgerekir.  
+
+# create yeni veri oluşturma
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+
+    # Default is fscohort/student_form.html.
+    template_name = "student/student_add.html" 
+
+    # This field is to redirect:
+    success_url = reverse_lazy("list")   
+
+# update işlemi
+
+class StudentUpdateView(UpdateView):
+        model = Student
+        form_class = StudentForm
+        template_name = "student/student_update.html"
+        success_url = reverse_lazy("list")
+        # pk_url_kwarg = 'id'
+
+class StudentDeleteView(DeleteView):
+      model = Student
+      template_name = "student/student_delete.html"
+      success_url = reverse_lazy("list")

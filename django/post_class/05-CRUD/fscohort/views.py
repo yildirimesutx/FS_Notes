2ab9eadc42b586bfdbb3e87ad86a914965ecc56f
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-06-03 12:47:28 +0300

    update and delete page

--- django/post_class/05-CRUD/fscohort/views.py
+++ django/post_class/05-CRUD/fscohort/views.py
@@ -33,3 +33,39 @@ def student_add(request):
     } 
 
     return render(request, "fscohort/student_add.html", context) 
+
+
+def student_update(request, id):
+    student =Student.objects.get(id=id) 
+    form = StudentForm(instance=student)
+    
+    if request.method =='POST':
+        form= StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "fscohort/student_update.html",context) 
+
+def student_delete(request, id):
+    student= Student.objects.get(id=id)
+    if request.method == 'POST':
+       student.delete() 
+       return redirect("list") 
+
+    return render(request, 'fscohort\student_delete.html') 
+
+
+
+def student_detail(request, id):
+    student = Student.objects.get(id=id)
+
+    context = {
+        'student' :student
+    }
+
+    return render(request, 'fscohort/student_detail.html', context)

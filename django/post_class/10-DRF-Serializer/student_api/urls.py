54d5d77729a3bbfdabcff28c0d7eab9c97856a3d
yildirimesutx <93818776+yildirimesutx@users.noreply.github.com>
2022-06-13 16:58:08 +0300

    DRF Serializer and View

---
+++ django/post_class/10-DRF-Serializer/student_api/urls.py
@@ -0,0 +1,30 @@
+
+# from django.urls import path
+# from .views import student_api, student_api_get_update_delete
+
+# urlpatterns = [
+    
+#     path('students/', student_api),
+#     path('students/<int:pk>/',student_api_get_update_delete)
+# ]
+
+# from django.urls import path
+# from .views import Student, StudentDetail
+
+
+# urlpatterns = [
+#     path("students/", Student.as_view(), name="list"),
+#     path('students/<int:id>/', StudentDetail.as_view(), name="detail"),
+# ]
+
+
+
+
+from django.urls import path
+from .views import StudentList, StudentGetUpdateDelete
+
+urlpatterns = [
+    path("students/", StudentList.as_view(), name="list"),
+    path("students/<int:id>/", StudentGetUpdateDelete.as_view(), name="detail"),
+
+]

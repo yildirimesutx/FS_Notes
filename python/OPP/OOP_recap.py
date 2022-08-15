5c2fb23923b1099049ebde3f12235dd3be488646
mesut <yildirimesutcw@gmail.com>
2022-08-15 22:48:13 +0300

    oop class and object -1

--- python/OPP/OOP_recap.py
+++ python/OPP/OOP_recap.py
@@ -25,17 +25,36 @@
 # aşağıda Personel class ını daha moduler hale getiiriyoruz, içerisine alması gereken attributleri yazdık, self ise sonradan tanımlayacağımız instance temsil ediyor.
 
 class Personel:
-    def __int__(self, isim, soyisim, maas):
+    def __init__(self, isim, soyisim, maas):
         self.isim = isim
         self.soyisim = soyisim
         self.maas = maas
+        self.eposta = f'{isim.lower()}.{soyisim.lower()}@firmam.com'
 
+    def  tam_isim(self):   #sonradan dahil ettik, yeni nesnelerin bilgilerine ulaşmak için bir method tanımladık.
+        return f'{self.isim} {self.soyisim}'   
 
-per_1  = Personel(isim = "John", soyisim = "Smith", maas = 30000)
+#self türetilecek nesneyi ifade eder. self yerine başka ifade de yazılabilir fakat bestpractive
+
+# self.ad = isim olarakda yazılabilir. per_1.ad olarak çağrılmalıdır
+
+per_1 = Personel(isim = "John", soyisim = "Smith", maas = 30000)
 per_2 = Personel("Mary", "Smith", 35000)
 
 print(per_1.isim)
 print(per_1.soyisim)
+print(per_1.maas)
+print(per_1.eposta)
+
+print(per_2.eposta)
+
+print(f'{per_1.isim} {per_1.soyisim}')
+
+# burada yeni instanceların bilgilerine ulaşmak için teker teker yazmaktanda class içinde method tanımlarız.
+
+print(per_1.tam_isim()) #methodu çağırabilmek için () ile kullanılmalı
+
+print(Personel.tam_isim(per_1)) #yukarıdaki ifade ile aaynı değeri döndürdü, argüman olarak class içine tanımladık
 
 
 
